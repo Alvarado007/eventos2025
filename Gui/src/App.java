61f48d19b5129943359824893af91c2c79072969
Alvarado007 <samuel.alvarado@correounivalle.edu.co>
2025-03-27 11:57:41 -0500

    se trabaj en la clase 7.1 y como se empiza con la GUi y como se implementa de forma sencilla en el main vamos a usar la extension JFrame y una cosa muy importante que debe tener el constructo es setDefaultCloseOperation para que las ventanas se cierren y evitar que se llene la memoria ram asignada a java

--- Gui/src/App.java
+++ Gui/src/App.java
@@ -1,5 +1,14 @@
-public class App {
+import javax.swing.JFrame;
+
+public class App extends JFrame {
+    public App() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Hello, World!");
+        setSize(800, 600);
+        setVisible(true);
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        App app = new App();
     }
 }
